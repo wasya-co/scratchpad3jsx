c32114ddaddfaf4fb5481f1b6c9a341d655fd469
mac_a2141 <victor@piousbox.com>
2023-03-08 13:57:35 -0500

    it works, im able to write and play an mp3 file.

--- src/App2.jsx
+++ src/App2.jsx
@@ -6,7 +6,6 @@ import {NativeAudio} from '@capacitor-community/native-audio'
 import write_blob from "capacitor-blob-writer";
 import React, { useEffect } from 'react'
 
-
 /**
  * Usage: logg(someObject, 'label')
  *
@@ -35,12 +34,12 @@ const logg = (a, b="", c=null) => {
 **/
 const App2 = (props) => {
 
-  const mp3Url = 'https://manager.piousbox.com/test1.mp3'
+  const fileName = 'test2.mp3'
+  const mp3Url = `https://manager.piousbox.com/${fileName}`
 
   const writeFile = async () => {
 
     let blob
-    let out
 
     blob = await fetch(mp3Url
     ).then( res => res.blob()
@@ -51,8 +50,8 @@ const App2 = (props) => {
 
 
     write_blob({
-      path: "test1.mp3",
-      directory: Directory.Data,
+      path: fileName,
+      directory: Directory.External,
       blob: blob,
       fast_mode: true,
       recursive: true,
@@ -60,15 +59,67 @@ const App2 = (props) => {
         logg(error, 'error 566')
       }
     }).then(function () {
-      console.log("Video written.");
+      logg("Video written.");
     });
+  }
+
+  const readFile = async () => {
+
+    let mp3Uri = await Filesystem.getUri({
+      path: fileName,
+      directory: Directory.External,
+    })
+    mp3Uri = mp3Uri.uri
+    logg(mp3Uri, 'uri')
+
+    NativeAudio.preload({
+      assetId: "fire",
+      assetPath: mp3Uri,
+      // audioChannelNum: 1,
+      isUrl: true,
+    })
+
+  }
+
+  useEffect(() => {
+    // writeFile()
+    readFile()
+  }, [])
+
+  const handleClick = () => {
+    NativeAudio.play({
+      assetId: 'fire',
+    });
+  }
+
+  return <>
+    <a onClick={handleClick}>abba 3</a>
+    <div className="App">
+
+    </div>
+  </>
+}
+export default App2
+
+
+
+
+
+
+
+
+
+// <audio controls autoPlay >
+//   <source src={mp3Url} type="audio/mpeg" />
+// </audio>
+
 
     // const reader = new FileReader()
     // await reader.readAsDataURL(blob)
     // reader.onloadend = function() {
     //   var base64data = reader.result;
     //   const writeRes = Filesystem.writeFile({
-    //     path: 'test1.mp3',
+    //     path: fileName,
     //     // data: base64data,
     //     data: blob,
     //     // directory: Directory.Data,
@@ -90,107 +141,4 @@ const App2 = (props) => {
     // })
 
 
-  }
 
-  const readFile = async () => {
-    const contents = await Filesystem.readFile({
-      path: 'test1.mp3',
-      // directory: Directory.Data,
-    }).catch(err => {
-      logg(err, "could not readFile")
-    })
-
-    logg(contents.data, 'contents.data')
-    const reader = new FileReader()
-    const read = await reader.readAsDataURL(contents.data)
-    reader.onloadend = function() {
-      var base64data = reader.result;
-      logg(base64data, 'loaded the base64data');
-    }
-    logg(read, 'read 333')
-
-  };
-
-  // useEffect(() => {
-  //   writeFile()
-  // }, [])
-
-  // readFile()
-
-  NativeAudio.preload({
-    assetId: "fire",
-    assetPath: `${Directory.Data}/test1.mp3`,
-    audioChannelNum: 1,
-    // isUrl: true,
-  });
-
-  const handleClick = () => {
-    NativeAudio.play({
-      assetId: 'fire',
-    });
-  }
-  setTimeout(() => {
-    NativeAudio.play({
-      assetId: 'fire',
-    });
-  }, 5 * 1000)
-
-  return <>
-    <a onClick={handleClick}>abba 3</a>
-    <div className="App">
-      { /* <audio controls autoPlay >
-        <source src={mp3Url} type="audio/mpeg" />
-      Your browser does not support the audio element.
-      </audio> */ }
-    </div>
-  </>
-}
-export default App2
-
-/*
-
-  const writeSecretFile = async () => {
-    await Filesystem.mkdir({
-      path: 'file:///var/mobile/Containers/Data/Application/22A433FD-D82D-4989-8BE6-9FC49DEA20BB',
-      recursive: true,
-    })
-    await Filesystem.writeFile({
-      path: 'secrets/text.txt',
-      data: "This is a test",
-      directory: Directory.Documents,
-      encoding: Encoding.UTF8,
-    });
-  };
-
-
-
-  const deleteSecretFile = async () => {
-    await Filesystem.deleteFile({
-      path: 'secrets/text.txt',
-      directory: Directory.Documents,
-    });
-  };
-
-  const readFilePath = async () => {
-    // Here's an example of reading a file with a full file path. Use this to
-    // read binary data (base64 encoded) from plugins that return File URIs, such as
-    // the Camera.
-    const contents = await Filesystem.readFile({
-      path: 'file:///var/mobile/Containers/Data/Application/22A433FD-D82D-4989-8BE6-9FC49DEA20BB/Documents/text.txt'
-    });
-
-    console.log('data:', contents);
-  };
-
-
-  const readDir = async () =>  {
-    let out
-    out = await Filesystem.readdir({
-      path: '.',
-      directory: Directory.Data,
-    })
-    console.log('readDir out1:', out.inspect)
-    logg(out, 'readdir out2')
-  }
-
-**/
